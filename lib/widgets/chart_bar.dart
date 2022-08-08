810b20f1adcaa85810238f4dcbfbe19c83f229e4
PaulTA91 <parmst202@caledonian.ac.uk>
2022-08-08 13:30:08 +0100

    creating bars

---
+++ lib/widgets/chart_bar.dart
@@ -0,0 +1,48 @@
+import 'package:flutter/material.dart';
+
+class ChartBar extends StatelessWidget {
+  final String label;
+  final double spendingAmount;
+  final double spendingPercentOfTotal;
+
+  ChartBar(this.label, this.spendingAmount, this.spendingPercentOfTotal);
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        Text('£${spendingAmount.toStringAsFixed(0)}'),
+        SizedBox(
+          height: 4,
+        ),
+        Container(
+          height: 60,
+          width: 10,
+          child: Stack(
+            children: [
+              Container(
+                decoration: BoxDecoration(
+                  border: Border.all(color: Colors.grey, width: 1.0),
+                  color: Color.fromRGBO(220, 220, 200, 1),
+                  borderRadius: BorderRadius.circular(20),
+                ),
+              ),
+              FractionallySizedBox(
+                heightFactor: spendingPercentOfTotal,
+                child: Container(
+                  decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(20),
+                      color: Theme.of(context).colorScheme.primary),
+                ),
+              ),
+            ],
+          ),
+        ),
+        SizedBox(
+          height: 4,
+        ),
+        Text(label),
+      ],
+    );
+  }
+}
